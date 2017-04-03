ff3f85045c0139166d405456899095590983ddcf
EragonVile <msubhaditya@gmail.com>
2017-04-03 11:33:11 +0400

    Create TIckTock.py

---
+++ TypingSpeed/TIckTock.py
@@ -0,0 +1,18 @@
+import time
+m = int(time.strftime('%M'))
+s = int(time.strftime('%S'))
+ti = m*60 + s
+r = raw_input('Type ')
+mm = int(time.strftime('%M'))
+ss = int(time.strftime('%S'))
+tf = mm*60 + ss
+Time = tf-ti
+
+def Len(r):
+	c = 0
+	for a in r:
+		c += 1
+	return c
+print Len(r)
+print Time
+print 'Speed = ', Len(r)/Time , ' char/sec (- a few characters)'
