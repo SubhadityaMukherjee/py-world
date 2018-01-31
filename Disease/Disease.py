3b6fc0bbb4d4e17f0b5da97fce05e0417685996a
Subhaditya Mukherjee <msubhaditya@gmail.com>
2018-01-31 23:40:05 +0530

    Removed disease

--- Disease/Disease.py
+++
@@ -1,9 +0,0 @@
-import csv,os
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-%matplotlib inline
-
-os.chdir(os.getcwd())
-data = pd.read_csv('fd-export.csv')
-print(data.plot())
